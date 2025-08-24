c91d19b7832badb51522f599c9a1f5557840eb2d
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-08-24 17:16:23 -0500

    Time: 44 ms (73.19%) | Memory: 20.1 MB (10.48%) - LeetSync

---
+++ binary-tree/142-linked-list-cycle-ii/linked-list-cycle-ii.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow = head
+        fast = head
+        hsh = set()
+        while slow:
+            if slow in hsh:
+                break
+            hsh.add(slow)
+            slow = slow.next
+        else:
+            return None
+        
+        # print(slow.val)
+
+        return slow
+        
