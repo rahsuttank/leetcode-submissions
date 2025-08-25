bef83c25df478461c240bedd755602b3cc2c2bad
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-08-25 18:48:18 -0500

    Time: 15 ms (42.59%) | Memory: 20.8 MB (21.17%) - LeetSync

--- binary-tree/498-diagonal-traverse/diagonal-traverse.py
+++ binary-tree/498-diagonal-traverse/diagonal-traverse.py
@@ -1,17 +1,11 @@
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         rows, cols = len(mat), len(mat[0])
-        # deq = deque()
-        # dirs = [[1, 0], [0, 1]]
-        # deq.append(mat[0][0])
         ans = []
-        # while deq:
         dirA = defaultdict(list)
         for r in range(rows):
             for c in range(cols):
                 dirA[r+c].append(mat[r][c])
-
-        # print(dirA)
 
         for i in range(rows + cols - 1):
             if i % 2 != 0:
