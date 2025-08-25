0698a285aa657a63c86221eddab8fb3e73bbc13d
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-08-25 18:52:26 -0500

    Time: 7 ms (85.18%) | Memory: 19.7 MB (86.23%) - LeetSync

--- binary-tree/498-diagonal-traverse/diagonal-traverse.py
+++ binary-tree/498-diagonal-traverse/diagonal-traverse.py
@@ -2,15 +2,24 @@ class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
         rows, cols = len(mat), len(mat[0])
         ans = []
-        dirA = defaultdict(list)
-        for r in range(rows):
-            for c in range(cols):
-                dirA[r+c].append(mat[r][c])
+        
+        for d in range(rows + cols - 1):
+            temp = []
+            # starting row for this diagonal
+            r = 0 if d < cols else d - cols + 1
+            # starting col for this diagonal
+            c = d if d < cols else cols - 1
 
-        for i in range(rows + cols - 1):
-            if i % 2 != 0:
-                ans.extend(dirA[i])
+            # collect elements in this diagonal
+            while r < rows and c >= 0:
+                temp.append(mat[r][c])
+                r += 1
+                c -= 1
+            
+            # reverse on even diagonals
+            if d % 2 == 0:
+                ans.extend(temp[::-1])
             else:
-                ans.extend(dirA[i][::-1])
+                ans.extend(temp)
         
         return ans
