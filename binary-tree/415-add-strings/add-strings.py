8ca9773139f155ef689b301269f5483af5f2678d
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-08-26 15:12:47 -0500

    Time: 6 ms (49.77%) | Memory: 18.4 MB (23.63%) - LeetSync

--- binary-tree/415-add-strings/add-strings.py
+++ binary-tree/415-add-strings/add-strings.py
@@ -1,10 +1,8 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
         carry = 0
-        l1 = len(num1)
-        l2 = len(num2)
-        s1 = l1 - 1
-        s2 = l2 - 1
+        s1 = len(num1) - 1
+        s2 = len(num2) - 1
         ans = []
         while s1 >= 0 or s2 >= 0 or carry:
             digit1 = int(num1[s1]) if s1 >= 0 else 0
