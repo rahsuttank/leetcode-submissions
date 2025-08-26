8e6e04111ce15793ab88f28fc63c256c4695a974
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-08-26 15:14:33 -0500

    Time: 3 ms (86.88%) | Memory: 18.3 MB (37.22%) - LeetSync

--- binary-tree/415-add-strings/add-strings.py
+++ binary-tree/415-add-strings/add-strings.py
@@ -5,8 +5,8 @@ class Solution:
         s2 = len(num2) - 1
         ans = []
         while s1 >= 0 or s2 >= 0 or carry:
-            digit1 = int(num1[s1]) if s1 >= 0 else 0
-            digit2 = int(num2[s2]) if s2 >= 0 else 0
+            digit1 = ord(num1[s1]) - ord("0") if s1 >= 0 else 0
+            digit2 = ord(num2[s2]) - ord("0") if s2 >= 0 else 0
             total = digit1 + digit2 + carry
             carry = total // 10
             ans.append(str(total % 10))
