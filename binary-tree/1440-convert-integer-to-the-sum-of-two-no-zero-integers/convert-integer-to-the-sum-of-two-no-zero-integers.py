1bd3e24ededdd75b542dc19f4496ebc0295c3ae4
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-07 21:21:01 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (83.63%) - LeetSync

--- binary-tree/1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
+++ binary-tree/1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.py
@@ -2,7 +2,7 @@ class Solution:
     def getNoZeroIntegers(self, n: int) -> List[int]:
         # ans = []
         def hasZero(num):
-            return "0" in set(str(num))
+            return "0" in str(num)
         for i in range(1, n):
             if not hasZero(n - i) and not hasZero(i):
                 return [i, n-i]
