0317c80d245c953f8e74250f566e5ccaee5dfdb7
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-11 19:38:00 -0500

    Time: 36 ms (39.70%) | Memory: 18.2 MB (31.82%) - LeetSync

--- binary-tree/3462-vowels-game-in-a-string/vowels-game-in-a-string.py
+++ binary-tree/3462-vowels-game-in-a-string/vowels-game-in-a-string.py
@@ -1,9 +1,8 @@
 class Solution:
     def doesAliceWin(self, s: str) -> bool:
-        vowels = "aeiou"
         vCt = 0
         for c in s:
-            if c in vowels:
+            if c in "aeiou":
                 vCt += 1
 
         if vCt == 0:
