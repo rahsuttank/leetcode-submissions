4bd98370bc8308fe39082818a4973bd747a9b6bf
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-15 21:44:08 -0500

    Time: 37 ms (93.57%) | Memory: 22.5 MB (54.15%) - LeetSync

--- binary-tree/1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
+++ binary-tree/1320-remove-all-adjacent-duplicates-in-string-ii/remove-all-adjacent-duplicates-in-string-ii.py
@@ -2,14 +2,11 @@ class Solution:
     def removeDuplicates(self, s: str, k: int) -> str:
         stk = []
         for c in s:
-            if stk:
-                if c == stk[-1][0]:
-                    stk[-1][1] += 1
-                    if stk[-1][1] == k:
-                        stk.pop()
-                else:
-                    stk.append([c, 1])
+            if stk and c == stk[-1][0]:
+                stk[-1][1] += 1
+                if stk[-1][1] == k:
+                    stk.pop()
             else:
                 stk.append([c, 1])
-        return "".join([x[0] if x[1] == 1 else x[1]*x[0] for x in stk])
+        return "".join([x * cnt for x, cnt in stk])
                     
