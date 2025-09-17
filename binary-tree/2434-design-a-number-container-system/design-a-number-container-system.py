e02490f5401ce4cb7095b3a9458d8a63b35b6457
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-16 22:08:58 -0500

    Time: 180 ms (45.00%) | Memory: 82.2 MB (38.85%) - LeetSync

---
+++ binary-tree/2434-design-a-number-container-system/design-a-number-container-system.py
@@ -0,0 +1,27 @@
+class NumberContainers:
+
+    def __init__(self):
+        self.num_heaps = defaultdict(list)
+        self.idx_num = {}
+
+        
+
+    def change(self, index: int, number: int) -> None:
+        heapq.heappush(self.num_heaps[number], index)
+        self.idx_num[index] = number
+        
+
+    def find(self, number: int) -> int:
+        heap = self.num_heaps[number]
+        if not heap:
+            return -1
+        while heap and self.idx_num[heap[0]] != number:
+            heapq.heappop(heap)
+        return heap[0] if heap else -1
+        
+
+
+# Your NumberContainers object will be instantiated and called as such:
+# obj = NumberContainers()
+# obj.change(index,number)
+# param_2 = obj.find(number)
