66a22367e818a0e907f9376d0794cdf4e5c3cbf8
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-16 22:24:15 -0500

    Time: 395 ms (5.13%) | Memory: 63.4 MB (7.48%) - LeetSync

--- binary-tree/1955-seat-reservation-manager/seat-reservation-manager.py
+++ binary-tree/1955-seat-reservation-manager/seat-reservation-manager.py
@@ -2,14 +2,12 @@ class SeatManager:
 
     def __init__(self, n: int):
         self.reserve_heap = []
-        # self.res = [0] * n
         for i in range(n):
             self.reserve_heap.append([0, i])
         
 
     def reserve(self) -> int:
         _, seat = heapq.heappop(self.reserve_heap)
-        # self.res[seat] = 1
         heapq.heappush(self.reserve_heap, [1, seat])
         return seat+1
 
