6f7c006b8e0e4e9e376a9b6f9cfe6e72bd7fa086
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-16 22:58:56 -0500

    Time: 31 ms (65.02%) | Memory: 27.6 MB (29.06%) - LeetSync

--- binary-tree/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.py
+++ binary-tree/1956-maximum-element-after-decreasing-and-rearranging/maximum-element-after-decreasing-and-rearranging.py
@@ -3,11 +3,9 @@ class Solution:
         arr.sort()
         arr[0] = 1
         for i in range(1, len(arr)):
-            if arr[i] - arr[i - 1] <= 1:
-                continue
-            else:
+            if arr[i] - arr[i - 1] > 1:
                 arr[i] = arr[i-1] + 1
-        
+                
         return arr[-1]
 
         
