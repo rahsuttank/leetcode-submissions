459882d831b0b4091abb1140158419e5bf5b0c0c
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-18 09:06:57 -0500

    Time: 404 ms (97.36%) | Memory: 98.6 MB (95.09%) - LeetSync

--- binary-tree/3678-design-task-manager/design-task-manager.py
+++ binary-tree/3678-design-task-manager/design-task-manager.py
@@ -1,43 +1,113 @@
 class TaskManager:
 
     def __init__(self, tasks: List[List[int]]):
-        self.task_map = {}
-        self.task_heap = []
+        self.tasks = []
+        self.valid = {}
+
         for user, task, priority in tasks:
-            self.task_map[task] = [priority, user, 1]
-            heapq.heappush(self.task_heap, [- priority, -task])
+            t = (-priority, -task, user)
+            self.tasks.append(t)
+            self.valid[-task] = t
         
+        heapify(self.tasks)
+       
 
     def add(self, userId: int, taskId: int, priority: int) -> None:
-        self.task_map[taskId] = [priority, userId, 1]
-        heapq.heappush(self.task_heap, [-priority, -taskId])
+        t = (-priority, -taskId, userId)
+        self.valid[-taskId] = t
+        heappush(self.tasks, t)
         
 
     def edit(self, taskId: int, newPriority: int) -> None:
-        # cur = self.task_map[taskId]
-        self.task_map[taskId][0] = newPriority
-        heapq.heappush(self.task_heap, [-newPriority, -taskId])
+        old_pri, old_task, old_user = self.valid[-taskId]
+        t = (-newPriority, old_task, old_user)
+        self.valid[-taskId] = t
+        heappush(self.tasks, t)
 
     def rmv(self, taskId: int) -> None:
-        # cur = self.task_map[taskId]
-        del self.task_map[taskId]
-        
+        del self.valid[-taskId]
 
     def execTop(self) -> int:
-        while self.task_heap:
-            priority, taskId = self.task_heap[0]
-            taskId, priority = -taskId, -priority
-            if (taskId in self.task_map and 
-                self.task_map[taskId][0] == priority and 
-                self.task_map[taskId][2] == 1):
-                heapq.heappop(self.task_heap)
-                userId = self.task_map[taskId][1]
-                del self.task_map[taskId]   # remove after execution
-                return userId
-            heapq.heappop(self.task_heap)  # discard stale entry
+        # print(self.tasks)
+        # print(self.valid)
+        while self.tasks:
+            pri, tsk, usr = heappop(self.tasks)
+
+            if tsk not in self.valid:
+                continue
+            
+           
+            if self.valid[tsk][0] != pri or self.valid[tsk][2] != usr:
+                continue
+            
+            del self.valid[tsk]
+            return usr
+            
         return -1
+
+
+# Your TaskManager object will be instantiated and called as such:
+# obj = TaskManager(tasks)
+# obj.add(userId,taskId,priority)
+# obj.edit(taskId,newPriority)
+# obj.rmv(taskId)
+# param_4 = obj.execTop()
+# class TaskManager:
+
+#     def __init__(self, tasks: List[List[int]]):
+#         self.tasks_dict = defaultdict(list)
+#         self.tasks = []
+#         for userId, taskId, priority in tasks:
+#             self.tasks_dict[-taskId] = [userId, -priority]
+#             self.tasks.append([-priority, -taskId, userId])
+        
+#         heapify(self.tasks)
+
+#     def add(self, userId: int, taskId: int, priority: int) -> None:
+#         self.tasks_dict[-taskId] = [userId, -priority]
+#         heappush(self.tasks, [-priority, -taskId, userId])
+        
+        
+#     def edit(self, taskId: int, newPriority: int) -> None:
+#         self.tasks_dict[-taskId][1] = -newPriority
+#         heappush(self.tasks, [-newPriority, -taskId, self.tasks_dict[-taskId][0]])
         
 
+#     def rmv(self, taskId: int) -> None:
+#         del self.tasks_dict[-taskId]
+        
+
+#     def execTop(self) -> int:
+#         while self.tasks:
+#             priority, taskId, userId = heappop(self.tasks)
+            
+#             if taskId not in self.tasks_dict: continue
+#             if self.tasks_dict[taskId][1] != priority or self.tasks_dict[taskId][0]!=userId: continue
+
+#             del self.tasks_dict[taskId]      
+#             return userId
+        
+#         return -1
+        
+
+"""  
+0: TaskManager - [[[1, 101, 10], [2, 102, 20], [3, 103, 15]]]       -> None  
+1: add - [4, 104, 5]                                                -> None  
+2: edit - [102, 8]                                                  -> None  
+3: execTop - []                                                     -> 3  
+4: rmv - [101]                                                      -> None  
+5: add - [5, 105, 15]                                               -> None  
+6: execTop - []                                                     -> 5  
+
+
+cmd = 
+var = 
+out = 
+mine = []
+for i in range(len(cmd)):
+    print(f'{i}: {cmd[i]} - {var[i]}        -> {out[i]}  ') #vs {mine[i]}
+    # if out[i] != mine[i]: print('THIS ONE ^^') 
+"""
 
 # Your TaskManager object will be instantiated and called as such:
 # obj = TaskManager(tasks)
