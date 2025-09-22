4839795e364ec38cc94ac5b78c64b1cc5f8e3f7d
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-21 21:12:37 -0500

    Time: 3 ms (34.62%) | Memory: 17.8 MB (34.33%) - LeetSync

--- binary-tree/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
+++ binary-tree/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.py
@@ -1,11 +1,5 @@
 class Solution:
-    def maxFrequencyElements(self, nums: List[int]) -> int:
-        ct = Counter(nums)
-        maxF = max(ct.items(), key = lambda x: x[1])[1]
-        ans = 0
-        for key, f in ct.items():
-            if f == maxF:
-                ans += f
-        
-        return ans
-        
+    def maxFrequencyElements(self, nums):
+        freq = Counter(nums)
+        maxf = max(freq.values())
+        return sum(v for v in freq.values() if v == maxf)
