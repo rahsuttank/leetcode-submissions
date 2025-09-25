c9e6325fff0bddadf458074dd9205c00b58cfb28
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-24 19:32:27 -0500

    Time: 7 ms (33.70%) | Memory: 19.7 MB (16.70%) - LeetSync

--- binary-tree/120-triangle/triangle.py
+++ binary-tree/120-triangle/triangle.py
@@ -2,13 +2,11 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
         dp = [[float("-inf")] * len(triangle[x]) for x in range(len(triangle))]
-        # print(dp)
         dp[0][0] = triangle[0][0]
         for i in range(1, n):
             l = len(triangle[i-1])
             for j in range(len(triangle[i])):
                 if 0 < j < l:
-                    print(j)
                     dp[i][j] = min(dp[i-1][j], dp[i-1][j-1]) + triangle[i][j]
                 elif j == 0:
                     dp[i][j] = dp[i-1][j]+ triangle[i][j]
