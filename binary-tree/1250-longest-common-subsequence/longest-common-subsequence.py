6d10febad2fccdb3aeb17fca78dd0a12648a4348
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-29 17:01:15 -0500

    Time: 507 ms (44.26%) | Memory: 43.7 MB (24.95%) - LeetSync

--- binary-tree/1250-longest-common-subsequence/longest-common-subsequence.py
+++ binary-tree/1250-longest-common-subsequence/longest-common-subsequence.py
@@ -9,8 +9,6 @@ class Solution:
                     dp[i][j] = dp[i-1][j-1] + 1
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-
-        print(dp)
                     
         return dp[-1][-1]
 
