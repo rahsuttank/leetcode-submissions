ea5cb8834fae3deb286e2dee9b7bfe12a4a532b8
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-29 22:19:51 -0500

    Time: 19 ms (96.74%) | Memory: 18.1 MB (41.80%) - LeetSync

--- binary-tree/2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.py
+++ binary-tree/2324-find-triangular-sum-of-an-array/find-triangular-sum-of-an-array.py
@@ -1,13 +1,9 @@
 class Solution:
     def triangularSum(self, nums: List[int]) -> int:
-        newNums = []
-        while len(nums) != 1:
-            for i in range(len(nums) - 1):
-                val = (nums[i] + nums[i+1]) % 10
-                newNums.append(val)
-            
-            nums = newNums
-            newNums = []
-        
-        return nums[0]
-        
+        length = len(nums)
+        coefficient = 1
+        result = 0
+        for i in range(length):
+            result += nums[i] * coefficient
+            coefficient = coefficient * (length - 1 - i) // (i + 1)
+        return result % 10
