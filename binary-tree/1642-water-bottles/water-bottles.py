ba29e7505bd68f491236c9246f7101f7668006bb
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-09-30 21:21:35 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.26%) - LeetSync

--- binary-tree/1642-water-bottles/water-bottles.py
+++ binary-tree/1642-water-bottles/water-bottles.py
@@ -1,13 +1,4 @@
 class Solution:
     def numWaterBottles(self, numBottles: int, numExchange: int) -> int:
-        if numExchange >= 2:
-            return (numBottles + (numBottles - 1) // (numExchange - 1))
-        # else:
-        #     ans = numBottles
-        #     drink = numBottles
-        #     while drink >= numExchange:
-        #         ans += (drink // numExchange)
-        #         drink = (drink % numExchange) + (drink // numExchange)
-
-        #     return ans 
+        return (numBottles + (numBottles - 1) // (numExchange - 1)) 
 
