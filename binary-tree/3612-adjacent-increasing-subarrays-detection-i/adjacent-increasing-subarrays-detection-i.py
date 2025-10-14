0b22dac3ebb562880b63f76a8a4a27cc8c064447
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-10-14 11:59:08 -0500

    Time: 89 ms (36.90%) | Memory: 17.8 MB (64.71%) - LeetSync

---
+++ binary-tree/3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hasIncreasingSubarrays(self, nums: List[int], k: int) -> bool:
+        ct = k - 1
+        if ct == 0:
+            return True
+        
+        for i in range(k + 1, len(nums)):
+            if nums[i] > nums[i - 1] and nums[i-k] > nums[i-k-1]:
+                ct -= 1
+                if ct == 0:
+                    return True
+
+            else:
+                ct = k - 1
+
+        return False
+
+        
+
+        
