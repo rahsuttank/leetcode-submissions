a1b933fe1b3914e6ffc2fda73853906b661fd3ba
Tushar Tank <42729671+rahsuttank@users.noreply.github.com>
2025-10-26 15:51:29 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (88.86%) - LeetSync

---
+++ binary-tree/1817-calculate-money-in-leetcode-bank/calculate-money-in-leetcode-bank.py
@@ -0,0 +1,14 @@
+class Solution:
+    def totalMoney(self, n: int) -> int:
+        weeks = n//7
+        # print(weeks)
+        rem_days = n%7
+        sum_ = 0
+        i = 28
+        for x in range(weeks):
+            sum_ += i
+            i += 7
+        # print(sum_)
+        sum_ += (rem_days/2)*(2*(weeks+1)+(rem_days-1))
+
+        return int(sum_)
